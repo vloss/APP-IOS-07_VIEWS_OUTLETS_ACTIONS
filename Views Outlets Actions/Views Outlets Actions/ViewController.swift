fe80310a1d829773aec53ef338bda6cfb79fdcec
vloss <viniciusvianaloss@gmail.com>
2023-01-13 18:13:37 -0300

    Finalização de revisão de Views, Outlets e Actions.

--- Views Outlets Actions/Views Outlets Actions/ViewController.swift
+++ Views Outlets Actions/Views Outlets Actions/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func doSomething(_ sender: Any, forEvent event: UIEvent) {
+        print("doSomething")
+    }
 
+    @IBAction func runSomething(_ sender: Any) {
+        print("runSomething")
+    }
+    
+    @IBAction func change(_ sender: Any) {
+        print("Change")
+        
+        // Caso necessário acessar o tipo do item
+        //(sender as! UISwitch).isOn
+    }
+    
 }
 
